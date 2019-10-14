79ed96a0a4eb662fe53fd1e8b91cea0bbb6ee9cf
asif ansari <ashifa454@gmail.com>
2019-10-14 15:14:47 +0530

    Webpack config resolved

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,94 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/asifansari/PdfToImage/main.js: Identifier 'PdfToImageGenerator' has already been declared (3:13)\n\n\u001b[0m \u001b[90m 1 | \u001b[39m\u001b[36mimport\u001b[39m \u001b[33mPdfToImageGenerator\u001b[39m from \u001b[32m\"./src\"\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 2 | \u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 3 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mconst\u001b[39m \u001b[33mPdfToImageGenerator\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   | \u001b[39m             \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n    at Parser.raise (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:6420:17)\n    at ScopeHandler.checkRedeclarationInScope (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:3776:12)\n    at ScopeHandler.declareName (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:3742:12)\n    at Parser.checkLVal (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:8159:22)\n    at Parser.parseVarId (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10682:10)\n    at Parser.parseVar (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10657:12)\n    at Parser.parseVarStatement (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10479:10)\n    at Parser.parseStatementContent (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10076:21)\n    at Parser.parseStatement (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10009:17)\n    at Parser.parseExportDeclaration (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:11194:17)\n    at Parser.maybeParseExportDeclaration (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:11150:31)\n    at Parser.parseExport (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:11079:29)\n    at Parser.parseStatementContent (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10113:27)\n    at Parser.parseStatement (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10009:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10585:25)\n    at Parser.parseBlockBody (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:10572:10)\n    at Parser.parseTopLevel (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:9940:10)\n    at Parser.parse (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:11447:17)\n    at parse (/Users/asifansari/PdfToImage/node_modules/@babel/parser/lib/index.js:11483:38)\n    at parser (/Users/asifansari/PdfToImage/node_modules/@babel/core/lib/transformation/normalize-file.js:168:34)\n    at normalizeFile (/Users/asifansari/PdfToImage/node_modules/@babel/core/lib/transformation/normalize-file.js:102:11)\n    at runSync (/Users/asifansari/PdfToImage/node_modules/@babel/core/lib/transformation/index.js:44:43)\n    at runAsync (/Users/asifansari/PdfToImage/node_modules/@babel/core/lib/transformation/index.js:35:14)\n    at process.nextTick (/Users/asifansari/PdfToImage/node_modules/@babel/core/lib/transform.js:34:34)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+
+/***/ })
+/******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,22 @@ const path = require("path");
 
 module.exports = {
   entry: "./main.js",
+  mode: "none",
   output: {
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
