21a840e34cd42cf6c2a5be649386e568a9a9aa55
asif ansari <ashifa454@gmail.com>
2019-10-14 15:03:03 +0530

    Convert method added

---
+++ main.js
@@ -0,0 +1,3 @@
+import PdfToImageGenerator from "./src";
+
+export const PdfToImageGenerator;
--- src/Factory/pdfFactory.js
+++ src/Factory/pdfFactory.js
@@ -4,7 +4,7 @@ import { ALL_PAGES } from "../constants";
 export const pdfFactory = function(fileURL) {
   this.fileURL = fileURL;
 };
-pdfFactory.prototype.renderToCanvas = function(pageNumber) {
+pdfFactory.prototype.convert = function(pageNumber) {
   return new Promise((resolve, reject) => {
     pdfjsLib.getDocument({ url: this.fileURL }).then(pdfDoc => {
       const totalPages = pdfDoc.numPages;
@@ -14,8 +14,23 @@ pdfFactory.prototype.renderToCanvas = function(pageNumber) {
         reject("pageNumber cannot be greater then totalPages");
       }
       for (let i = 0; i < pageNumber; i++) {
-          new FileIO()
+        rendertToCanvas(pdfDoc.getPage(i + 1));
       }
     });
   });
 };
+/**
+ *
+ * @param page
+ */
+async function rendertToCanvas(page) {
+  const CANVAS = document.createElement("canvas");
+  const CONTEXT = CANVAS.getContext("2d");
+  const renderContext = {
+    canvasContext: CONTEXT,
+    viewport: page.getViewport()
+  };
+  await page.render(renderContext);
+  const imageURL = CANVAS.toDataURL("image/png");
+  return new FileIO(imageURL).toFile();
+}
